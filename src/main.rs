a4455a551586e550113aafbb6e819486bf476186
Lyxsm <>
2025-12-20 14:07:25 +0100

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -2,17 +2,31 @@
 use std::io::{self, Write};
 
 fn main() {
-    // Prints "$" to terminal
-    print!("$ ");
-    io::stdout().flush().unwrap();
+    repl();
+}
 
-    // Reads user input and stores it in the "command" variable
-    let mut command = String::new();
-    io::stdin().read_line(&mut command).unwrap();
+fn repl() {
+    //let mut b = true;
+    //while b {
+    while true {
+        // Prints "$" to terminal
+        print!("$ ");
+        io::stdout().flush().unwrap();
+    
+        // Reads user input and stores it in the "command" variable
+        let mut command = String::new();
+        io::stdin().read_line(&mut command).unwrap();
 
-    match command {
-        _ => {
-            println!("{}: command not found", command.replace("\n", "").replace("\r", "").replace("\r\n", ""));
+        command = command.replace("\n", "").replace("\r", "").replace("\r\n", "");
+
+        match command.as_str() {
+            //"exit" => {
+            //    b = false;
+            //    println!("test");
+            //},
+            _ => {
+                println!("{}: command not found", command);
+            }
         }
     }
 }
