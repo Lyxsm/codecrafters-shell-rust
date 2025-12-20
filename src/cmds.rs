17daf3f1b4aa1c31fd3fdec96ee1e1768ac6f86d
Lyxsm <>
2025-12-20 17:03:19 +0100

    codecrafters submit [skip ci]

---
+++ src/cmds.rs
@@ -0,0 +1,62 @@
+#![allow(unused)]
+#[derive(PartialEq)]
+pub enum CmdType {
+	Builtin,
+	Valid,
+	Invalid,
+}
+#[derive(PartialEq)]
+pub enum CmdAction {
+	Print(String),
+	Terminate,
+	Type(String),
+	None,
+}
+
+#[derive(PartialEq)]
+pub enum Eval {
+	CmdType,
+	CmdAction,
+}
+
+pub fn eval_cmd(input: &str) -> (CmdAction, CmdType, String) {
+	let (cmd, args) = cmd_split(input);
+	let cmd_action = cmd_action(cmd, args.clone());
+	let cmd_type = cmd_type(cmd);
+
+	(cmd_action, cmd_type, args)
+}
+
+pub fn cmd_type(cmd: &str) -> CmdType {
+	match cmd {
+		"echo" => CmdType::Builtin,
+		"exit" => CmdType::Builtin,
+		"type" => CmdType::Builtin,
+		"none" => CmdType::Invalid,
+		_ => CmdType::Invalid,
+	}
+}
+
+pub fn cmd_action(cmd: &str, args: String) -> CmdAction {
+	match cmd {
+		"echo" => CmdAction::Print(args),
+		"exit" => CmdAction::Terminate,
+		"type" => CmdAction::Type(args),
+		"none" => CmdAction::None,
+		_ => CmdAction::Print(format!("{cmd}: command not found")),
+	}
+}
+
+pub fn cmd_split(input: &str) -> (&str, String) {
+	let mut split = input.split(" ");
+
+	let cmd = if let Some(cmd) = split.next() {
+		cmd
+	} else {
+		return ("none", "none".to_string());
+	};
+
+	let args = split.collect::<Vec<&str>>().join(" ");
+	println!("cmd: {}; args: {}", cmd, args);
+	(cmd, args)
+}
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,14 @@
 #[allow(unused_imports)]
 use std::io::{self, Write};
 
+mod cmds;
+
 fn main() {
     repl();
 }
 
 fn repl() {
-    let mut b = true;
-    while b {
+    loop {
         // Prints "$" to terminal
         print!("$ ");
         io::stdout().flush().unwrap();
@@ -15,19 +16,21 @@ fn repl() {
         // Reads user input and stores it in the "command" variable
         let mut command = String::new();
         io::stdin().read_line(&mut command).unwrap();
+        let command = command.trim();
 
-        if command.starts_with("echo") {
-            let input = command.strip_prefix("echo ").unwrap().trim();
-            println!("{}", input);
-        } else {
-            match command.trim() {
-                "exit" => {
-                    b = false;
-                },
-                cmd => {
-                    println!("{cmd}: command not found");
+        let (action, cmd_type, args) = cmds::eval_cmd(command);
+        match action {
+            cmds::CmdAction::Terminate => break,
+            cmds::CmdAction::Print(str) => println!("{str}"),
+            cmds::CmdAction::Type(str) => {
+                let arg_type = cmds::cmd_type(str.as_str());
+                if arg_type == cmds::CmdType::Invalid {
+                    println!("{str}: not found");               
+                } else {
+                    println!("{str} is a shell {}", if cmd_type == cmds::CmdType::Builtin {"builtin"} else {"poop"});
                 }
             }
+            cmds::CmdAction::None => println!("{command}: command not found"),
         }
     }
 }
