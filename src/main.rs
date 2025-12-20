a906a3a71535d9b0c879dab6da8b9a77ae301a52
Lyxsm <>
2025-12-20 13:42:00 +0100

    message: command not found

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,17 @@
 use std::io::{self, Write};
 
 fn main() {
-    // TODO: Uncomment the code below to pass the first stage
+    // Prints "$" to terminal
     print!("$ ");
     io::stdout().flush().unwrap();
+
+    // Reads user input and stores it in the "command" variable
+    let mut command = String::new();
+    io::stdin().read_line(&mut command).unwrap();
+
+    match command {
+        _ => {
+            println!("{}: command not found", command.replace("\n", "").replace("\r", "").replace("\r\n", ""));
+        }
+    }
 }
